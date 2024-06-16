5b99e2b8d46b16d5ec71d46cbe9c84f3a8618bef
jiyagaba <122517714+jiyagaba@users.noreply.github.com>
2024-06-16 15:39:37 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"Physics"<<endl;
+    cout<<"Wallah"<<endl;
+    return 0;
+}
---
+++ 5.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int radius = 10;
+    double circumference = 2 * 3.14 * radius;
+    cout<<"CIrcumference is:"<<circumference;
+    return 0;
+}
